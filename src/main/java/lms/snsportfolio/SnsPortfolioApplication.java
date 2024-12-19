f41a23c7125246081252b659eadd37bf2bd6ab9c
iamipro <iamipro@naver.com>
2024-12-19 19:57:15 +0900

    Initial commit

---
+++ src/main/java/lms/snsportfolio/SnsPortfolioApplication.java
@@ -0,0 +1,13 @@
+package lms.snsportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lms/snsportfolio/SnsPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package lms.snsportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
